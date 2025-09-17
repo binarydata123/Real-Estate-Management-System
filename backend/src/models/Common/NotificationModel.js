802c165b0fef03343850e3cb8939ed30d2306d01
Binary Data <binarydata.sale@gmail.com>
2025-09-17 12:15:01 +0530

    [s]: add filter and capitalize file name

--- backend/src/models/Common/NotificationModel.js
+++ backend/src/models/Common/NotificationModel.js
@@ -8,22 +8,15 @@ const notificationSchema = new mongoose.Schema(
       required: true,
     },
     agencyId: {
-<<<<<<< HEAD
       type: mongoose.Schema.Types.ObjectId,
-      ref: "Agent",
+      ref: "Agency",
       required: true,
-=======
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Agency',
-        required: true
->>>>>>> 1b6466c4eea3d4af416f1ac6529ba0e1018590a3
     },
     message: {
       type: String,
       required: true,
     },
     type: {
-<<<<<<< HEAD
       type: String,
       enum: [
         "welcome",
@@ -31,13 +24,9 @@ const notificationSchema = new mongoose.Schema(
         "task_assigned",
         "meeting_scheduled",
         "property_updated",
+        "property_added",
       ],
       default: "welcome",
-=======
-        type: String,
-        enum: ['welcome', 'new_lead', 'task_assigned', 'meeting_scheduled', 'property_updated', 'property_added'],
-        default: 'welcome'
->>>>>>> 1b6466c4eea3d4af416f1ac6529ba0e1018590a3
     },
     read: {
       type: Boolean,
