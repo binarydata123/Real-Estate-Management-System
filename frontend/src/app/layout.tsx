412f69ddfb8f4c375c3b3e02c4f974c9ab36059e
binarydata123 <binarydata.sale@gmail.com>
2025-09-22 09:30:16 +0530

    working on edit property functionality

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -33,17 +33,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ToastProvider>
-<<<<<<< HEAD
           <AuthProvider>
             {children}
           </AuthProvider>
           <SpeakMessage />
         </ToastProvider>
-=======
-          <AuthProvider>{children}</AuthProvider>
-        </ToastProvider>
-        <SpeakMessage />
->>>>>>> d6dd75f8fc3bf16a671d043f1a0fc2dcff61ac1d
       </body>
     </html>
   );
