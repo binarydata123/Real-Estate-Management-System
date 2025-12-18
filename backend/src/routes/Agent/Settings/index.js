5ec77edf283144a05d216f821e06264f56b9533e
binarydata123 <binarydata.sale@gmail.com>
2025-12-18 14:59:48 +0530

    Fixed get-settings Bug

--- backend/src/routes/Agent/Settings/index.js
+++ backend/src/routes/Agent/Settings/index.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { getAgencySettings, updateAgencySettings } from "../../../controllers/Agent/SettingsController.js";
+import { protect } from "../../../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.post("/update-settings", updateAgencySettings);
-router.get("/get-settings", getAgencySettings);
+router.post("/update-settings", protect(['agent']) ,updateAgencySettings);
+router.get("/get-settings", protect(['agent']) ,getAgencySettings);
 
 export default router;
